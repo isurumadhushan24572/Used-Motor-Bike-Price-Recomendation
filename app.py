f16cea6d2b8b61d3828cf5b8c6e4825cdced8a21
Isuru <108188969+Isrur@users.noreply.github.com>
2025-02-11 21:53:43 +0530

    commit to main

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
-
 
 app = Flask(__name__)
 
@@ -9,15 +8,15 @@ with open('predictor.pickle', 'rb') as f:
     best_model = pickle.load(f)
 
 province_list = ['Central','Eastern','North Central','North Western','Northern','Sabaragamuwa','Southern','Uva','Western']
-def province_encoding(provinces):
-            list_provice = []
-            for province in province_list:
-                if provinces == province:
-                    list_provice.append(1)
-                else:
-                    list_provice.append(0)
 
-            return list_provice
+def province_encoding(provinces):
+    list_provice = []
+    for province in province_list:
+        if provinces == province:
+            list_provice.append(1)
+        else:
+            list_provice.append(0)
+    return list_provice
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -35,7 +34,27 @@ def home():
         y_pred_new1 = best_model.predict([new_data])
         pred_value = y_pred_new1[0]
 
-    return render_template('index.html', pred_value = pred_value)
+    return render_template('index.html', pred_value=pred_value)
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    try:
+        data = request.get_json()
+        mileage = int(data['Mileage'])
+        published_year = int(data['published_year'])
+        manufactured_year = int(data['manufactured_year'])
+        province = data['province']
+
+        p_list = province_encoding(province)
+        new_data = [mileage, published_year, manufactured_year]
+        new_data.extend(p_list)
+
+        y_pred_new1 = best_model.predict([new_data])
+        pred_value = y_pred_new1[0]
+
+        return jsonify({'prediction': pred_value})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
